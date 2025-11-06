1f16498025a402d3cc7b8c87dcabaa76cedfae8e
Brendan Devine <brendantdevine@gmail.com>
2025-11-06 11:15:23 -0600

    feat: wrap

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Wrapping is nice for both .md files and code for resizing windows.
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = { "shift:2", "min:20" }
