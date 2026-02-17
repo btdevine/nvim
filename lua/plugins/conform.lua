4065adf861cfb9e2498125282e2cf25811dd5a09
Brendan Devine <brendantdevine@gmail.com>
2026-02-17 14:55:27 -0600

    feat: strip weird line separators

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,10 +5,21 @@ return {
       timeout_ms = 500,
       lsp_format = "fallback",
     },
+    formatters = {
+      -- Strip U+2028 (Line Separator) and U+2029 (Paragraph Separator)
+      -- This can cause some weird behavior with TypeScript
+      -- where the compiler ignores these and resultantly
+      -- the line numbers gets screwed up. Best to just remove them on write.
+      strip_unusual_line_terminators = {
+        command = "sed",
+        args = { "-e", "s/\xe2\x80\xa8//g", "-e", "s/\xe2\x80\xa9//g" },
+        stdin = true,
+      },
+    },
     formatters_by_ft = {
       lua = { "stylua" },
-      typescript = { "prettier" },
-      typescriptreact = { "prettier" },
+      typescript = { "strip_unusual_line_terminators", "prettier" },
+      typescriptreact = { "strip_unusual_line_terminators", "prettier" },
       rust = { "rustfmt" },
     },
   },
