1695e9e9a984e58ba0adf8975a517929a98c60d3
Brendan Devine <brendantdevine@gmail.com>
2026-03-06 17:26:01 +0000

    feat: mason tool installer

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,19 @@
 return {
   "williamboman/mason.nvim",
   opts = {},
+  dependencies = {
+    -- Auto-install tools on load
+    -- Found from: https://github.com/mason-org/mason.nvim/issues/103
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      opts = {
+        ensure_installed = {
+          "rust-analyzer",
+          "typescript-language-server",
+          "prettier",
+          "eslint-lsp",
+        },
+      },
+    },
+  },
 }
